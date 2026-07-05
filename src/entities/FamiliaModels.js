755d07b0bc821f5ed4b2acaf8dda8c0f9ce06b20
pepicn <jose.carrillo2401@alumnos.ubiobio.cl>
2026-07-05 18:27:01 -0400

    Correcion de doble validacion

--- src/entities/FamiliaModels.js
+++ src/entities/FamiliaModels.js
@@ -64,13 +64,7 @@ const Familia = sequelize.define('Familia', {
   timestamps: true,
   createdAt: 'created_at',
   updatedAt: 'updated_at',
-  indexes: [
-    {
-      unique: true,
-      fields: ['calle', 'numero','comuna', 'region'],
-      name: 'unica_direccion'
-    }
-  ]
+  
 });
 
 // ─────────────────────────────────────────────
